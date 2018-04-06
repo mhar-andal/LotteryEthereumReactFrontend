fadca3cf48703f3e12165e58860c5325a1e7c2d6
Mhar Andal <mharandal96@gmail.com>
2018-04-06 11:57:34 -0500

    :sparkles:Adds functionality to enter the lottery with an ethereum value:sparkles:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import web3 from './web3';
+
 import './App.css';
+import lottery from './lottery';
 
 class App extends Component {
+  state = {
+    balance: '',
+    manager: '',
+    message: '',
+    players: [],
+    value: '',
+  };
+
+  async componentDidMount() {
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+
+    this.setState({ balance, manager, players });
+  }
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const { value } = this.state;
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether'),
+    });
+
+    this.setState({
+      message: 'You have been entered!',
+    });
+  };
+
   render() {
+    const { balance, manager, message, players } = this.state;
+    const etherPrize = web3.utils.fromWei(balance, 'ether');
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by: {manager}</p>
+        <p>
+          There are currently {this.state.players.length} people entered,
+          competing to win {etherPrize} ether!
         </p>
+        <hr />
+
+        <h2>{message}</h2>
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luch?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              onChange={event =>
+                this.setState({
+                  value: event.target.value,
+                })
+              }
+              value={this.state.value}
+            />
+          </div>
+
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
---
+++ src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from './web3';
+
+const address = '0xaa8963a6f17f3d3216dc3f79255518c814f6969c';
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
